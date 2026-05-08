a247cc077b3d9d59d3f103cfc6867fc3fa2fe1d0
Jonas <livejo0045@gmail.com>
2026-05-08 17:40:55 +0100

    content area

---
+++ .py
@@ -0,0 +1,20 @@
+# content area
+content= qvboxlayout()
+content.setspacing(3)
+content.setcontentsmargins(0,10,0,10)
+
+if self.clip.content_type == "image" and self.clip.image:
+    pixmap = qpixmap.fromimage(self.clip.image).scaledtohight(48, qt.transformationmode.smoothtransformation)
+    img_label = qlabel()
+    img_label.setpixmap(pixmap)
+    img_label.setstylesheet("border-radius:4px;")
+content.addwidget(img_label) else:
+self.text_label =qlabel (self.clip.preview oe "(empty)")
+self.text_label.setwordwrap(false)
+self.text_labe;.setmaximumwidth(340)
+font = qfont("consolas",11)
+self.text_label.setfont(font) 
+
+self.text_label.setstylesheet(f"color:{text_primary}; background:transparent;")
+content.addwidget(self.text_label)
+
